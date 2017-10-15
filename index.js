856c2fb7545bf5de01524e4a80de5e8aebd1506b
Breck Yunits <breck7@gmail.com>
2017-10-15 15:43:01 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const TreeProgram = require("treeprogram")
+const fs = require("fs")
+const FireGrammar = fs.readFileSync(__dirname + "/fire.grammar", "utf8")
+
+class FireProgram extends TreeProgram {
+  getGrammarString() {
+    return FireGrammar
+  }
+
+  async execute() {
+    let outputLines = []
+    const _originalConsoleLog = console.log
+    const tempConsoleLog = (...params) => outputLines.push(params)
+    console.log = tempConsoleLog
+    eval(this.compile())
+    console.log = _originalConsoleLog
+    console.log(outputLines.join("\n"))
+    return outputLines
+  }
+}
+
+module.exports = FireProgram
